69922ca4fe2c8adff4b9a86e2d30567a27c4dfe2
IDStack <idstack@outlook.co.id>
2020-03-25 01:31:08 +0700

    create add post component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "bootstrap/dist/js/bootstrap.bundle.min";
 import PostAdd from "./components/post/add.component";
 import PostShow from "./components/post/show.component";
 import PostIndex from "./components/post/index.component";
-import { Router, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 class App extends Component {
   render() {
@@ -15,14 +15,14 @@ class App extends Component {
       <Router>
         <div>
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">Navbar</a>
+            <Link className="navbar-brand" to={"#"}></Link>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNav">
               <ul className="navbar-nav">
                 <li className="nav-item active">
-                  <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                  <Link className="nav-link" to={"#"}>Home <span className="sr-only">(current)</span></Link>
                 </li>
                 <li className="nav-item">
                   <Link to={"/posts"} className="nav-link">
@@ -40,9 +40,9 @@ class App extends Component {
 
           <div className="container mt-3">
             <Switch>
-              <Route exact path={["/", "/posts"]} component={PostIndex} />
+              {/* <Route exact path={["/", "/posts"]} component={PostIndex} /> */}
               <Route exact path="/add" component={PostAdd} />
-              <Route exact path="/posts/:id" component={PostShow} />
+              {/* <Route exact path="/posts/:id" component={PostShow} /> */}
             </Switch>
           </div>
         </div>
---
+++ src/components/post/add.component.js
@@ -0,0 +1,96 @@
+import React, { Component } from "react";
+import PostService from "../../services/post.service";
+
+export default class PostAdd extends Component {
+    constructor(props) {
+        super(props);
+        this.onChangeTitle = this.onChangeTitle.bind(this);
+        this.onChangeDescription = this.onChangeDescription.bind(this);
+        this.savePost = this.savePost.bind(this);
+        this.clearPost = this.clearPost.bind(this);
+
+        this.state = {
+            id: null,
+            title: "",
+            description: "",
+            published: false
+        }
+    }
+
+    onChangeTitle(e) {
+        this.setState({
+            title: e.target.value
+        });
+    }
+
+    onChangeDescription(e) {
+        this.setState({
+            description: e.target.value
+        });
+    }
+
+    clearPost() {
+        this.setState({
+            id: null,
+            title: "",
+            description: "",
+            published: false,
+      
+            submitted: false
+        });
+    }
+
+    savePost() {
+        let data = {
+            title: this.state.title,
+            description: this.state.description
+        };
+
+        PostService.create(data)
+            .then((result) => {
+                this.clearPost();
+                console.log(result.data);
+            }).catch((err) => {
+                console.log(err);
+            });
+
+        this.setState({
+            submitted: true
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <div className="form-group">     
+                    <label>Title</label>
+                    <input
+                        type="text"
+                        className="form-control"
+                        id="title"
+                        required
+                        value={this.state.title}
+                        onChange={this.onChangeTitle}
+                        name="title"
+                    />
+                </div>
+                <div className="form-group">
+                    <label>Description</label>
+                    <textarea
+                        type="text"
+                        className="form-control"
+                        id="description"
+                        cols="10" rows="4"
+                        required
+                        value={this.state.description}
+                        onChange={this.onChangeDescription}
+                        name="description"
+                    />
+                </div>
+                <button onClick={this.savePost} className="btn btn-success">
+                    Submit
+                </button>
+            </div>
+        )
+    }
+}
