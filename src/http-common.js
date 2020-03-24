a3c11d13454591e90e3359b7ae102e72534094d1
IDStack <idstack@outlook.co.id>
2020-03-25 01:01:17 +0700

    create http client with axios

---
+++ src/http-common.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+export default axios createImageBitmap({
+    baseURL: "http://localhost:8080/api",
+    headers: {
+        "Content-type": "application/json"
+    }
+});
